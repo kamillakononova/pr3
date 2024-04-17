d551e3a9b2f906687c810403d63deebca806e844
kamillakononova <165295335+kamillakononova@users.noreply.github.com>
2024-04-17 11:13:05 +0300

    Add files via upload

---
+++ rep1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form name="feedback" method="POST" action="rep1.php">
+        <label>Ввод данных</label><br>
+        <label></label><br>
+        <label>Имя: <input type="text" name="name"></label><br>
+<?
+   
+?>
+</body>
+</html>
